a5c85d554002ff225c9207943905034a63c299f3
Simon Klein <simon.klein@cloudogu.com>
2024-08-05 07:46:50 +0200

    #201 Update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [ require('@cloudogu/ces-theme-tailwind/tailwind.presets.cjs') ],
   content: [
       "app/src/main/resources/templates/**/*.html"
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 }
 
