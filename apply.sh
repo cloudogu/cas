71decde898cb58516d75b9941d4fafb96ff2763c
Simon Klein <simon.klein@cloudogu.com>
2024-08-07 11:49:51 +0200

    #201 Update design

--- apply.sh
+++ apply.sh
@@ -8,10 +8,12 @@ set -o pipefail
 docker exec -it cas bash -c "rm -rf /opt/apache-tomcat/webapps/cas/WEB-INF/classes/templates"
 docker exec -it cas bash -c "rm -rf /opt/apache-tomcat/webapps/cas/WEB-INF/classes/static/css"
 docker exec -it cas bash -c "rm -rf /opt/apache-tomcat/webapps/cas/WEB-INF/classes/static/js"
+docker exec -it cas bash -c "rm -rf /opt/apache-tomcat/webapps/cas/WEB-INF/classes/static/images"
 docker cp app/src/main/resources/templates cas:/opt/apache-tomcat/webapps/cas/WEB-INF/classes
 docker cp app/src/main/resources/cas-theme-default.properties cas:/opt/apache-tomcat/webapps/cas/WEB-INF/classes
 docker cp app/src/main/resources/static/css cas:/opt/apache-tomcat/webapps/cas/WEB-INF/classes/static/css
 docker cp app/src/main/resources/static/js cas:/opt/apache-tomcat/webapps/cas/WEB-INF/classes/static/js
+docker cp app/src/main/resources/static/images cas:/opt/apache-tomcat/webapps/cas/WEB-INF/classes/static/images
 docker cp app/src/main/resources/custom_messages_de.properties cas:/opt/apache-tomcat/webapps/cas/WEB-INF/classes/custom_messages_de.properties
 docker cp app/src/main/resources/custom_messages.properties cas:/opt/apache-tomcat/webapps/cas/WEB-INF/classes/custom_messages.properties
 docker kill cas
