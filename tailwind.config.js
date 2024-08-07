7e063a2fe8e64a932a894185e8ca1b4551d1f634
Simon Klein <simon.klein@cloudogu.com>
2024-08-07 15:22:52 +0200

    #201 Use tailwind style presets from theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   presets: [ require('@cloudogu/ces-theme-tailwind/tailwind.presets.cjs') ],
   content: [
-      "app/src/main/resources/templates/**/*.html"
+      "app/src/main/resources/templates/**/*.html",
   ],
 }
 
