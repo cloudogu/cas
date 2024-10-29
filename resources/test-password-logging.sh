1c021bce166788aafde57787ba854383ebb980de
Laurin Friebe <laurin.friebe-extern@cloudogu.com>
2024-10-29 07:45:30 +0100

    # 225 add logging to curl

--- resources/test-password-logging.sh
+++ resources/test-password-logging.sh
@@ -11,17 +11,18 @@ ADMIN_PW=Ecosystem2016!
 
 # get execution token for login
 curl 'https://192.168.56.2/cas/login' \
-  --data-raw "username=${ADMIN_USER}&${ADMIN_PW}&_eventId=submit&geolocation=&deviceFingerprint=" \
+  -v -L --data-raw "username=${ADMIN_USER}&${ADMIN_PW}&_eventId=submit&geolocation=&deviceFingerprint=" \
   --insecure > firstRequest
 
 EXECUTION_TOKEN=$(grep 'name="execution" value=' "firstRequest" | awk '{gsub("value=\"", "", $4); gsub("\"/><input", "", $4); print $4}')
 
 curl 'https://192.168.56.2/cas/login' -X POST \
-  --data-raw "username=${ADMIN_USER}&${ADMIN_PW}&execution=${EXECUTION_TOKEN}&_eventId=submit&geolocation=&deviceFingerprint=" \
+  -v -L --data-raw "username=${ADMIN_USER}&${ADMIN_PW}&execution=${EXECUTION_TOKEN}&_eventId=submit&geolocation=&deviceFingerprint=" \
   --insecure
 
 # check docker logs
 docker container logs cas >> cas_logs.txt
+# remove me later, just for testing
 echo "${PASSWORD}" >> cas_logs.txt
 if grep -q "${PASSWORD}" cas_logs.txt; then
   echo "ERROR: Found a non-encrypted password in the docker log file. "
