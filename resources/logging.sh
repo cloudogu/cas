753713a8713f1bea389e6853d567735cf38a0e2f
Jeremias Weber <jeremias.weber@cloudogu.com>
2025-02-12 14:16:22 +0100

    #250 Exit with error on invalid log level

--- resources/logging.sh
+++ resources/logging.sh
@@ -55,9 +55,8 @@ function validateDoguLogLevel() {
   doguctl validate "${DEFAULT_LOGGING_KEY}" || validateExitCode=$?
 
   if [[ ${validateExitCode} -ne 0 ]]; then
-      echo "${SCRIPT_LOG_PREFIX} WARNING: The loglevel configured in ${DEFAULT_LOGGING_KEY} is invalid."
-      echo "${SCRIPT_LOG_PREFIX} WARNING: Removing misconfigured value."
-      doguctl config --rm "${DEFAULT_LOGGING_KEY}"
+      echo "${SCRIPT_LOG_PREFIX} ERROR: The loglevel configured in ${DEFAULT_LOGGING_KEY} is invalid."
+      exit 1
   fi
 
   return
