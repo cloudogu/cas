568005a46c9a66f5a5b03f343acbe34d8ba6b8ab
Dennis Schwarzer <dennis.schwarzer@cloudogu.com>
2025-09-08 11:05:33 +0200

    test #33

--- integrationTests/cypress/support/step_definitions/oidc.js
+++ integrationTests/cypress/support/step_definitions/oidc.js
@@ -18,29 +18,13 @@ Then("a button for delegated OIDC authentication is shown", function () {
 });
 
 When("the user enters keycloak username", function () {
-    cy.get('input[id=username').type("tester")
+    cy.get('input[id=username]').type("tester")
 })
 
 When("the user enters keycloak password", function () {
-    cy.get('input[id=password').type("test")
+    cy.get('input[id=password]').type("test")
 })
 
 Then("login to keycloak", function () {
     cy.get('[id=kc-login]').click()
 });
-
-
-
-
-        cy.get('button[id="submit"]').click()
-
-<input tabindex="7" class="pf-c-button pf-m-primary pf-m-block btn-lg" name="login" id="kc-login" type="submit" value="Sign In">
-
-        // <input tabindex="2" id="username" class="pf-c-form-control" name="username" value="" type="text" autofocus="" autocomplete="username" aria-invalid="">
-
-// <input tabindex="3" id="password" class="pf-c-form-control" name="password" type="password" autocomplete="current-password" aria-invalid="">
-
-        // Given("the user is currently not logged in the CES", function () {
-//     cy.visit("/cas/logout")
-// });
-// https://34.89.172.118/cas/clientredirect?client_name=MyProvider
