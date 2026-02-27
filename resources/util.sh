c8a49179505dfa257af374e6931f7a63232265e0
Laurin Friebe <laurin.friebe-extern@cloudogu.com>
2026-02-27 14:35:23 +0100

    remove totp test keys

--- resources/util.sh
+++ resources/util.sh
@@ -169,9 +169,6 @@ function createTOTPEncryptionCodes() {
     SIGNING_KEY=$(java -jar /jars/jwk-gen.jar -t oct -s 512 | sed -n '/{/,/}/p' | jq -r '.k'); doguctl config "totp/signing_key" "$SIGNING_KEY"
     ENCRYPTION_KEY=$(java -jar /jars/jwk-gen.jar -t oct -s 512 | sed -n '/{/,/}/p' | jq -r '.k'); doguctl config "totp/encryption_key" "$ENCRYPTION_KEY"
     SCRATCH_CODES_ENCRYPTION_KEY=$(java -jar /jars/jwk-gen.jar -t oct -s 512 | sed -n '/{/,/}/p' | jq -r '.k'); doguctl config "totp/scratch_codes/encryption_key" "$SCRATCH_CODES_ENCRYPTION_KEY"
-#    doguctl config "totp/signing_key" "YRSU5V3penSandm3tZ3Vt3pTX230w_RZoE9uHhYfhWnhfouQ0mNfc8t7yGCBijqrluNyTDiHuua3nsMeAwBKGQ"
-#    doguctl config "totp/encryption_key" "AQTbyu3s3Kt3FbKtt3elYDu4gs1kRf3SdKRvV4wFGyFHXTnzMvsu5xtwalxn7gw_0K2ZBIk2DLjUFLI0pzigmg"
-#    doguctl config "totp/scratch_codes/encryption_key" "AQTbyu3s3Kt3FbKtt3elYDu4gs1kRf3SdKRvV4wFGyFHXTnzMvsu5xtwalxn7gw_0K2ZBIk2DLjUFLI0pzigmg"
   fi
 }
 
