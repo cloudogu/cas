c01e93ae3f10c96d0dba6b7309689c2024118bb0
Leon <mail@omel.dev>
2026-03-23 14:32:12 +0100

    remove unused multi-node skip logic from before.js

--- integrationTests/cypress/support/step_definitions/before.js
+++ integrationTests/cypress/support/step_definitions/before.js
@@ -2,19 +2,6 @@ const {
     Before,
 } = require("@badeball/cypress-cucumber-preprocessor");
 
-
-/**
- * Skip scenarios tagged with @skip_on_multinode when running in a multi-node pipeline.
- * Set the Cypress env variable "multiNode" to true to activate this behaviour.
- * This should only be used for tests which are not YET compatible with the multi-node pipeline, but will be adapted in
- * the future.
- */
-Before({tags: "@skip_on_multinode"}, function () {
-    if (Cypress.env("multiNode") === true) {
-        this.skip();
-    }
-});
-
 /**
  * Create a new test user which must change his password at next login
  */
